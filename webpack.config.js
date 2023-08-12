a1c48381b294ce98060ac9ab331e3b0e4c97fe9f
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-12 19:09:53 +0200

    Load menu.json5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const json5 = require('json5');
 
 module.exports = {
     mode: 'development',
@@ -11,4 +12,15 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+        ]
+    }
 };
