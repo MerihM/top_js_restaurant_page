f5f19a59371d47c4cb50cbfea94cbaf3db7a3c34
MerihM <103122972+MerihM@users.noreply.github.com>
2023-08-19 22:01:47 +0200

    Implement makeCard function

--- src/tabs.js
+++ src/tabs.js
@@ -1,3 +1,4 @@
+import data from './menu.json5';
 export function createTabList() {
     const tabDiv = document.createElement('div');
     const cities = ['London', 'Paris', 'Tokyo'];
@@ -40,5 +41,56 @@ export function openTab(tab) {
         cont.style.display = 'none';
         if ((tab.srcElement.innerHTML) == cont.id)
             cont.style.display = 'block'
+    }
+    let tempData = data.hasMenu.hasMenuSection[0].Italy
+    makeCard(tempData)
+}
+
+/* <div class="allCards tabcontent">
+    <div class="menuTitle">Current menu</div>
+    <div class="card">
+        <h3 class="title">Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, labore sint placeat
+            cum
+            aliquid enim odio,
+            voluptates tempora consequatur incidunt ex quos eligendi praesentium aspernatur ut sapiente natus eum
+            sequi.
+        </p>
+        <div class="priceContainer">
+            <span class="priceText">Price</span>
+            <span class="price">5.23$</span>
+        </div>
+    </div>
+</div> */
+function makeCard(data) {
+    for (let items of data) {
+        let allCards = document.createElement('div');
+        allCards.classList.add('allCards', 'tabcontent')
+        for (let i = 1; i < 11; i++) {
+            let item = items.MenuItems[`item${i}`]
+            let card = document.createElement('div');
+            let title = document.createElement('h3');
+            let description = document.createElement('p');
+            let priceContainer = document.createElement('div');
+            let priceText = document.createElement('span');
+            let price = document.createElement('span');
+            card.classList.add('card');
+            title.classList.add('menuTitle');
+            description.classList.add('description');
+            priceContainer.classList.add('priceContainer');
+            priceText.classList.add('priceText');
+            price.classList.add('price');
+            title.innerText = item.name;
+            description.innerText = item.description;
+            priceText.innerText = 'Price';
+            price.innerText = item.offers.price;
+            priceContainer.appendChild(priceText);
+            priceContainer.appendChild(price);
+            card.appendChild(title);
+            card.appendChild(description);
+            card.appendChild(priceContainer);
+            allCards.appendChild(card)
+        }
+        console.log(allCards)
     }
 }
